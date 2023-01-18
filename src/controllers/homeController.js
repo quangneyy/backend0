b56ec5065c73943087226a927cc0b2e5a4f01ebe
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-01-19 00:31:20 +0700

    #36

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,7 @@
 const connection = require("../config/database");
 
 const getHomepage = (req, res) => {
-  // process data
-  // call model
-  let users = [];
-
-  connection.query("select * from Users u", function (err, results, fields) {
-    users = results;
-    console.log(">>> results home page= ", results); // results contains rows returned by server
-
-    // console.log(">>> check users: ", users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
 const getQuangNey = (req, res) => {
