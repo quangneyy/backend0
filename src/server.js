44a2a43413f1a4bff50e0cd232cb5b59222fe487
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-01-18 15:18:40 +0700

    #30

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 
+const mysql = require("mysql2");
+
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hard . uat . prod
 const hostname = process.env.HOST_NAME;
@@ -14,6 +16,21 @@ configViewEngine(app);
 // khai bao route
 app.use("/", webRoutes);
 
-app.listen(port, () => {
+// test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, //default: 3306
+  user: "root", // default: empty
+  password: "123456",
+  database: "quangney",
+});
+
+// simple query
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(">>> results= ", results); // results contains rows returned by server
+  console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
+});
+
+app.listen(port, hostname, () => {
   console.log(`listening on port ${port}`);
 });
