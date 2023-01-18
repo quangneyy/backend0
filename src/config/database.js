ef8a385753df84e85c643a6d66f96653b469ce3b
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-01-18 15:31:10 +0700

    #31

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, //default: 3306
+  user: process.env.DB_USER, // default: empty
+  password: process.env.DB_PASSWORD, // default: empty
+  database: process.env.DB_NAME, // default: empty
+});
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const express = require("express"); //commonjs
 // import express from "express";// es modules
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hard . uat . prod
@@ -17,18 +16,10 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 // test connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307, //default: 3306
-  user: "root", // default: empty
-  password: "123456",
-  database: "quangney",
-});
 
 // simple query
 connection.query("select * from Users u", function (err, results, fields) {
   console.log(">>> results= ", results); // results contains rows returned by server
-  console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
