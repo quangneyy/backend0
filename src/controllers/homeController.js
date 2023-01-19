ddc003d9ac26a93fe3617ffabb65e8774937da18
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-01-19 11:02:07 +0700

    #38

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,8 +9,23 @@ const getQuangNey = (req, res) => {
 };
 
 const postCreateUser = (req, res) => {
-  console.log(">>> req.body: ", req.body);
-  res.send("create a new user");
+  let email = req.body.email;
+  let name = req.body.myname;
+  let city = req.body.city;
+
+  console.log(">>> req.body: ", email, name, city);
+
+  // let { email, name, city } = req.body;
+
+  connection.query(
+    `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log(results);
+
+      res.send("Created user succeed !");
+    }
+  );
 };
 
 module.exports = {
