dc9f00eaaebd2e76955ff38650927db6254a96ee
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-01 17:17:51 +0700

    #60

---
+++ src/models/Kitten.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+// shape data
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+module.exports = Kitten;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
-const mongoose = require("mongoose");
+const Kitten = require("./models/Kitten");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hard . uat . prod
@@ -20,11 +20,7 @@ configViewEngine(app);
 // khai bao route
 app.use("/", webRoutes);
 
-const kittySchema = new mongoose.Schema({
-  name: String,
-});
-const Kitten = mongoose.model("Kitten", kittySchema);
-const cat = new Kitten({ name: "Quang Ney cat" });
+const cat = new Kitten({ name: "Quang Ney cat model" });
 cat.save();
 
 (async () => {
