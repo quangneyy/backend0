b52b9f918d48a24c1c7ec2a65b53c1c4100895a4
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-01 16:26:47 +0700

    #59

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
+const mongoose = require("mongoose");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hard . uat . prod
@@ -18,6 +19,13 @@ configViewEngine(app);
 
 // khai bao route
 app.use("/", webRoutes);
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+const Kitten = mongoose.model("Kitten", kittySchema);
+const cat = new Kitten({ name: "Quang Ney cat" });
+cat.save();
 
 (async () => {
   try {
