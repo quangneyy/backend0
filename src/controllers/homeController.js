551bb7f9e2f20444e205f13a826298c965b079c3
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-02 15:53:52 +0700

    #64

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -58,16 +58,19 @@ const postUpdateUser = async (req, res) => {
 
 const postDeleteUser = async (req, res) => {
   const userId = req.params.id;
-  let user = await getUserById(userId);
-
+  let user = await User.findById(userId).exec();
   res.render("delete.ejs", { userEdit: user });
 };
 
 const postHandleRemoveUser = async (req, res) => {
   const id = req.body.userId;
 
-  await deleteUserById(id);
+  // await deleteUserById(id);
+  let result = await User.deleteOne({
+    _id: id,
+  });
 
+  console.log(">>> result: ", result);
   res.redirect("/");
 };
 
