996da2b121d103679f219d97fa8b0a6d9445ae00
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-03 12:01:36 +0700

    #81

---
+++ src/models/customer.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+// shape data
+const customerSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    address: String,
+    phone: String,
+    email: String,
+    image: String,
+    description: String,
+  },
+  { timestamps: true } // createAt, updateAt
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
