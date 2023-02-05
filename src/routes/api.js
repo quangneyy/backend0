1f19e137997fc6fd1b2a996c0906ec77279953b5
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-05 13:14:28 +0700

    #86

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,11 +11,15 @@ const {
   postUploadMultipleFilesAPI,
 } = require("../controllers/apiController");
 
+const { postCreateCustomer } = require("../controllers/customerController");
+
 routerAPI.get("/users", getUsersAPI);
 routerAPI.post("/users", postCreateUserAPI);
 routerAPI.put("/users", putUpdateUserAPI);
 routerAPI.delete("/users", deleteUserAPI);
 routerAPI.post("/file", postUploadSingleFileApi);
 routerAPI.post("/files", postUploadMultipleFilesAPI);
+
+routerAPI.post("/customers", postCreateCustomer);
 
 module.exports = routerAPI; //export default
