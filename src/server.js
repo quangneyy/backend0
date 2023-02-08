e9c46662a77c14bba597ce6886a1ad1fdebba054
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-02-08 22:33:50 +0700

    #104

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
 const fileUpload = require("express-fileupload");
-
 const connection = require("./config/database");
+const { MongoClient } = require("mongodb");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hard . uat . prod
@@ -19,7 +19,7 @@ app.use(fileUpload());
 app.use(express.json()); // for json
 app.use(express.urlencoded({ extended: true })); // for form data
 
-// config template en dgine
+// config template en engine
 configViewEngine(app);
 
 // khai bao route
@@ -28,7 +28,25 @@ app.use("/v1/api/", apiRoutes);
 
 (async () => {
   try {
-    await connection();
+    // using mongoose
+    // await connection();
+
+    // using mongodb driver
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+
+    // Database Name
+    const dbName = process.env.DB_NAME;
+
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const collection = db.collection("documents");
+
+    //
+
     app.listen(port, hostname, () => {
       console.log(`Backend zero app listening on port ${port}`);
     });
